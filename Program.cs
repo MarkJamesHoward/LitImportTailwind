496c918ee2e2915948f5945bf5a9b5d1e5c54d99
MarkJamesHoward <markjameshoward@hotmail.co.uk>
2023-05-20 14:28:00 +1200

    initial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string twFileInput = "tailwindGenerated.css";
+string twFileOutput = "tailwindForLit.ts";
+string twFolder = @"./";
+
+string twFileContents = File.ReadAllText(Path.Combine(twFolder, twFileInput));
+string twFileContentsSanitized = twFileContents.Replace("`", "");
+string template = $"import {{ css }} from \"lit\"; export const TWStyles = css`{twFileContentsSanitized}`";
+
+File.WriteAllText(Path.Combine(twFolder, twFileOutput), template);
+
